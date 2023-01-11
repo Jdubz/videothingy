f6f5adc08300851ac15e4a2979e6a41faaa4e897
Josh Wentworth <contact@joshwentworth.com>
2023-01-10 19:11:37 -0800

    cleanup

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,17 +1,15 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import Webcam from "react-webcam";
+import Webcam from 'react-webcam';
 
-import icon from '../../assets/icon.svg';
 import './App.css';
-
 
 const Hello = () => {
   return (
     <div>
       <h1>Video Thingy!</h1>
-      <Webcam
-        onUserMediaError={console.error}
-      />
+      <div>
+        <Webcam onUserMediaError={console.error} />
+      </div>
     </div>
   );
 };
